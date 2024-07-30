1932e89fb7837ba0f5d72824dae3b6efae890f44
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-07-30 12:34:23 +0200

    todolist

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,18 +1,33 @@
 
 import './App.css'
 import {useState} from 'react'
-
+import Cards from './Cards'
+// import Cards from './Cards'
 function App() {
   const[formData, setformData]= useState("")
-  const [displyed,setdisplyed]= useState("")
+  const [displyed,setdisplyed]= useState([])
   function handle(event){
-  setformData(event.target.value)
+    setformData(event.target.value)
   }
-  const handleclick = () =>{
-    setdisplyed(formData)
+  
+  const addTodo = ()=>{
+    let id= 1;
+    if(displyed.length>0){
+      id= displyed[0].id+1
+    }
+    let todo ={
+      id:id,
+      text:formData,
+      completed: false
+    }
+    let newTodos = [todo,...displyed]
+    setdisplyed(newTodos)
+    setformData("")
   }
-
-
+  function remove (){
+    
+  }
+  
   return (
     <>
     <section className='flex flex-col items-center '>
@@ -22,13 +37,15 @@ function App() {
       name="todo"
       onChange= {handle}
       value= {formData}
-      
       className=' w-[40rem] h-14 rounded-xl drop-shadow-lg relative'
       />
- <button className="fa-solid fa-plus absolute top-16 right-[30rem] bg-green-400 text-white h-6 w-6 rounded-full " onClick={ handleclick}></button>    
+ <button className="fa-solid fa-plus absolute top-16 right-[30rem] bg-green-400 text-white h-6 w-6 rounded-full " onClick={addTodo}></button>    
+     {displyed.map(todo =>(
+      < Cards key={todo.id} text={todo.text} remove={remove}/>
+     ))}
+     
+  
     </section>
-    {displyed&& 
-  <div><p className='px-20'>{displyed}</p></div>}
        
     
     </>
--- todolist/src/Cards.jsx
+++ todolist/src/Cards.jsx
@@ -1,4 +1,10 @@
 function Cards(prop){
-   <p>{prop.text}</p>  
+
+   return (
+    <div className=" bg-gray-300 w-[30rem] h-[3rem] mt-4 flex items-center rounded-xl drop-shadow-lg justify-between">
+       <p className="px-8">{prop.text}</p> 
+       <button className="px-5"onClick={prop.remove} ><i className="fa-solid fa-trash text-red-500"></i></button> 
+    </div>
+   )
 }
 export default Cards;
