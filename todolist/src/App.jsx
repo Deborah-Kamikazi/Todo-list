a471b69d93deda0f1941c6ef54730a592fd6e63d
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-07-31 09:50:30 +0200

    added other features in my todo

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,55 +1,63 @@
-
-import './App.css'
-import {useState} from 'react'
-import Cards from './Cards'
+import "./App.css";
+import { useState } from "react";
+import Cards from "./Cards";
+import {nanoid} from "nanoid";  
 // import Cards from './Cards'
+
+
 function App() {
-  const[formData, setformData]= useState("")
-  const [displyed,setdisplyed]= useState([])
-  function handle(event){
-    setformData(event.target.value)
+  const [formData, setformData] = useState("");
+  const [displyed, setdisplyed] = useState([]);
+  function handle(event) {
+    setformData(event.target.value);
   }
-  
-  const addTodo = ()=>{
-    let id= 1;
-    if(displyed.length>0){
-      id= displyed[0].id+1
-    }
-    let todo ={
-      id:id,
-      text:formData,
-      completed: false
-    }
-    let newTodos = [todo,...displyed]
-    setdisplyed(newTodos)
-    setformData("")
+
+  const addTodo = () => {
+    let todo = {
+      ids: nanoid(),
+      text: formData,
+      completed:false,
+    };
+    let newTodos = [todo, ...displyed];
+    setdisplyed(newTodos);
+    setformData("");
+  };
+  function remove(id) {    
+    setdisplyed(displyed.filter((todo) => todo.ids !== id));
   }
-  function remove (){
-    
+  function complete(id){
+    setdisplyed(displyed.map((todo) => todo.ids === id? {...todo, completed: !todo.completed } : todo));
+    console.log("completed");
   }
-  
+
   return (
     <>
-    <section className='flex flex-col items-center '>
-    <p className='text-5xl font-bold text-gray-300'>todos</p>
-     <input type="text"
-      placeholder=' Add todo..'
-      name="todo"
-      onChange= {handle}
-      value= {formData}
-      className=' w-[40rem] h-14 rounded-xl drop-shadow-lg relative'
-      />
- <button className="fa-solid fa-plus absolute top-16 right-[30rem] bg-green-400 text-white h-6 w-6 rounded-full " onClick={addTodo}></button>    
-     {displyed.map(todo =>(
-      < Cards key={todo.id} text={todo.text} remove={remove}/>
-     ))}
-     
-  
-    </section>
-       
-    
+      <section className="flex flex-col items-center ">
+        <p className="text-5xl font-bold text-gray-300">todos</p>
+        <input
+          type="text"
+          placeholder=" Add todo.."
+          name="todo"
+          onChange={handle}
+          value={formData}
+          className=" w-[40rem] h-14 rounded-xl drop-shadow-lg relative"
+        />
+        <button
+          className="fa-solid fa-plus absolute top-16 right-[30rem] bg-green-400 text-white h-6 w-6 rounded-full "
+          onClick={addTodo}
+        ></button>
+        {displyed.map((todo) => (
+          <Cards
+            key={todo.id}
+            text={todo.text}
+            remove={() => remove(todo.ids)}
+            complete={() => complete(todo.ids)}
+            className={todo.completed ? "underlined" : ""}
+          />
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- todolist/src/Cards.jsx
+++ todolist/src/Cards.jsx
@@ -1,9 +1,13 @@
-function Cards(prop){
+
+
+// eslint-disable-next-line react/prop-types
+function Cards({ text, remove, complete, className }){
 
    return (
-    <div className=" bg-gray-300 w-[30rem] h-[3rem] mt-4 flex items-center rounded-xl drop-shadow-lg justify-between">
-       <p className="px-8">{prop.text}</p> 
-       <button className="px-5"onClick={prop.remove} ><i className="fa-solid fa-trash text-red-500"></i></button> 
+      <div className={`bg-gray-300 w-[30rem] h-[3rem] mt-4 flex items-center rounded-xl drop-shadow-lg justify-evenly ${className}`}>
+      <input type="checkbox"  className="px-6" onClick={complete}/>
+       <p className="">{text}</p> 
+       <button className="px-9"onClick={remove} ><i className="fa-solid fa-trash text-red-500"></i></button> 
     </div>
    )
 }
